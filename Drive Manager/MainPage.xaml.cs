1b533b9a29c1e6dbf6e7eaf3d6ad100eeda8fbed
Andrew N <andrewcneal03@gmail.com>
2024-06-03 14:52:13 -0700

    Updated source code

--- Drive Manager/MainPage.xaml.cs
+++ Drive Manager/MainPage.xaml.cs
@@ -1,25 +1,33 @@
-﻿namespace Drive_Manager
+﻿using System.Collections.ObjectModel;
+
+public struct Drive
+{
+    char driveLetter;
+    string driveName;
+    string driveFormat;
+    long totalSize;
+    long sizeUsed;
+}
+
+namespace Drive_Manager
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
+}
 
+public class JobModel
+{
+        public ObservableCollection<string> Job { get; set; } = new ObservableCollection<string>()
+    {
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 5"
+    };
 }
---
+++ Drive Manager/Resources/DriveInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+public class DriveInfo 
+{
+    public char numberDrives {  get;}
+    public char[] driveLetters { get;}
+    public string[] driveNames { get;}
+    public long[] driveCapacity { get;}
+
+
+}
+
+public class DriveInfoView : StackLayout
+{
+    
+}
